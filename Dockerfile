793451cf5aaf38897d9f7c9e0dae787bb94c0c30
Hunter Dermott <hbdermott@gmail.com>
2023-02-24 20:07:13 -0800

    Dockerize
    Making container setup

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM --platform=linux/amd64 node:18-bullseye
+RUN wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+RUN sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+RUN apt-get -y update
+RUN apt-get install -y google-chrome-stable
+RUN apt-get install -yqq unzip
+RUN wget -O /tmp/chromedriver.zip http://chromedriver.storage.googleapis.com/`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`/chromedriver_linux64.zip
+RUN unzip /tmp/chromedriver.zip chromedriver -d /usr/local/bin/
+ENV DISPLAY=:99
+WORKDIR /data/app
+COPY . .
+ENV TOKEN="your_token_here"
+ENV CLIENT_ID="your_client_id"
+RUN yarn install
+RUN yarn build
+RUN chmod +x ./start.sh
+CMD ["./start.sh"]
---
+++ src/deploy.ts
@@ -0,0 +1,38 @@
+import * as fs from "fs";
+import * as path from "path";
+import { REST, Routes } from "discord.js";
+require("dotenv").config();
+
+const commandsPath = path.join(__dirname, "commands");
+
+const commands = [];
+
+const commandFiles = fs
+	.readdirSync(commandsPath)
+	.filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+
+(async () => {
+	try {
+		console.log(
+			`Started refreshing ${commands.length} application (/) commands.`
+		);
+
+		const data: any = await rest.put(
+			Routes.applicationCommands(process.env.CLIENT_ID),
+			{ body: commands }
+		);
+
+		console.log(
+			`Successfully reloaded ${data.length} application (/) commands.`
+		);
+	} catch (error) {
+		console.error(error);
+	}
+})();
